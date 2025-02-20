ace5cea7f93d42246cf2e32516a9c3a47c0a93aa
Alexandre Decan <alexandre.decan@lexpage.net>
2025-02-20 08:53:48 +0100

    Move methods to top-level so it can be adapted if needed

--- pcorr.py
+++ pcorr.py
@@ -56,22 +56,21 @@ def benjamini_hochberg(pv, alpha=0.05, *, sort=True):
             return p
     return None
 
+_methods = {
+    'None': no_correction,
+    'Bonferroni': bonferroni,
+    'Holm': holm,
+    'Hochberg': hochberg,
+    'Benjamini-Hochberg': benjamini_hochberg,
+}
 
 def maximal_pvalue(pv, alpha=0.05, *, sort=True):
     """ Print the highest p-value that is still significant after correction. """
     if sort:
         pv = sorted(pv)
-
-    methods = {
-        'None': no_correction,
-        'Bonferroni': bonferroni,
-        'Holm': holm,
-        'Hochberg': hochberg,
-        'Benjamini-Hochberg': benjamini_hochberg,
-    }
+        
     print(f'Context: {alpha=}, {len(pv)} p-values')
-
-    for method, fn in methods.items():
+    for method, fn in _methods.items():
         v = fn(pv, alpha, sort=False)
         n = len([x for x in pv if x <= v])
         print(f'{method:<20}\tp-value={v:.6f}\t{n:>3} significant p-values')
